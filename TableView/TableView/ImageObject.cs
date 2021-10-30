6bb2811d60f6b760b0214c975000a230690333b7
Nguyeenvuu <63364941+Nguyeenvuu@users.noreply.github.com>
2021-10-31 01:26:35 +0700

    add tableview

---
+++ TableView/TableView/ImageObject.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TableView
+{
+  public  class ImageObject
+    {
+        public string image { get; set; }
+        public string title { get; set; }
+    }
+}
